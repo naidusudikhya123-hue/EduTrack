5a86ce1fee0b740e155e1e4a944639811ef086ef
NaiduSudikhya <naidu.sudikhya@gmail.com>
2026-04-12 13:47:10 +0530

    added monitoring

--- course-service/src/main/java/com/course/CourseServiceApplication.java
+++ course-service/src/main/java/com/course/CourseServiceApplication.java
@@ -2,8 +2,10 @@ package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class CourseServiceApplication {
 
 	public static void main(String[] args) {
